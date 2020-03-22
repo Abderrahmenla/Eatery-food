b469d8f4a4117bfe2a053b85920218f51328eb53
armagedon <abdo.vic@hotmail.com>
2020-03-22 22:46:46 +0100

    background header

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,15 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { PageHeader, Banner } from "../utils"
+import img from "../images/bcg/contactBcg.jpeg"
 
 const ContactPage = () => (
   <Layout>
     <SEO title="Contact" />
+    <PageHeader img={img}>
+      <Banner title="contact us" subtitle="let's get in touch"></Banner>
+    </PageHeader>
   </Layout>
 )
 
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,10 +1,15 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { PageHeader, Banner } from "../utils"
+import img from "../images/bcg/menuBcg.jpeg"
 
 const MenuPage = () => (
   <Layout>
     <SEO title="Menu" />
+    <PageHeader img={img}>
+      <Banner title="menu" subtitle={`let's dig in`}></Banner>
+    </PageHeader>
   </Layout>
 )
 
